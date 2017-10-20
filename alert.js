14e1b9db15884379d5a053fa598f6caee7ff4028
Zebrick <zebrik1992@gmail.com>
2017-10-20 21:03:24 +0300

    lesson5

---
+++ alert.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var hello = 'Hello world!';
+
+var message;
+
+// скопировали значение
+message = hello;
+
+alert( hello ); // Hello world!
+alert( message ); // Hello world!
+
+var admin,name;
+name = 'Василий';
+admin = name;
+alert(admin);
